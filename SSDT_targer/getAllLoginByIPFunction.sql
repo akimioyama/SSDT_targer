4b99787c1a3edaee32a2172241136b37ac0747c1
Авраменко Дима <no.pers@bk.ru>
2023-06-13 21:28:14 +0300

    ssshhh***ttt

---
+++ SSDT_targer/getAllLoginByIPFunction.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[getAllLoginByIPFunction]
+(
+	@IP_address nvarchar(50)
+)
+RETURNS @InfoByIP_address TABLE
+(
+	[Id] INT,
+	[user_id] INT,
+	[last_login_time] DATETIMEOFFSET,
+	[device_settings] NVARCHAR(50)
+)
+AS
+BEGIN
+	INSERT @InfoByIP_address
+	SELECT [Id],[user_id],[last_login_time],[device_settings] 
+	FROM [dbo].[HistoryLogin] WHERE [HistoryLogin].Id = @IP_address
+	RETURN
+END
---
+++ SSDT_targer/getInfoLoginByIDProcedure.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[getInfoLoginByIDProcedure]
+	@id_user INT
+AS
+BEGIN
+	SELECT [Id], [last_login_time], [IP], [device_settings]
+	FROM [dbo].[HistoryLogin] 
+	WHERE [HistoryLogin].user_id = @id_user
+END
