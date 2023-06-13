8ae38d24af593b0bcca2652f876add19c0d77503
Авраменко Дима <no.pers@bk.ru>
2023-06-13 21:40:37 +0300

    uh

--- SSDT_targer/getAllLoginByIPFunction.sql
+++ SSDT_targer/getAllLoginByIPFunction.sql
@@ -5,12 +5,14 @@
 RETURNS @InfoByIP_address TABLE
 (
 	[login] NVARCHAR(50),
-	[Id] INT
+	[Id] INT,
+	[last_login_time] DATETIMEOFFSET,
+	[device_settings] NVARCHAR(50)
 )
 AS
 BEGIN
 	INSERT @InfoByIP_address
-	SELECT [User].[login], [User].[Id] 
+	SELECT [User].[login], [User].[Id], [HistoryLogin].[last_login_time], [HistoryLogin].[device_settings] 
 	FROM [dbo].[HistoryLogin] JOIN [dbo].[User] 
 	ON [HistoryLogin].user_id = [User].Id
 	WHERE [HistoryLogin].IP = @IP_address
